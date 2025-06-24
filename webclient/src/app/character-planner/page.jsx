5f99e4aece92342830afea15f430a8e268db1c07
direded <dmpalyonniy@gmail.com>
2025-06-24 22:00:55 +0300

    webclient feat: disable right click on character-planner page

--- webclient/src/app/character-planner/page.jsx
+++ webclient/src/app/character-planner/page.jsx
@@ -1,10 +1,16 @@
 'use client';
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import PlayerItems from '../../components/PlayerItems';
 import GameInfo from '../../components/GameInfo';
 
 export default function CharacterPlanner() {
+
+	useEffect(() => {
+		document.addEventListener('contextmenu', event => event.preventDefault());
+	}, []);
+
+
   const [characterData, setCharacterData] = useState({
     name: 'Exile',
     level: 1,
